bce5aead85efe963e3bc207ea84adf0fd2337f4c
Bethany Nicholson <blnicho@sandia.gov>
2021-04-23 14:28:55 -0600

    Trying again to fix book tests on pypy3

--- examples/pyomobook/test_book_examples.py
+++ examples/pyomobook/test_book_examples.py
@@ -232,13 +232,9 @@ def filter_file_contents(lines):
 
             # A few substitutions to get tests passing on pypy3
             if ".inf" in i:
-                i.replace(".inf", "inf")
-                filtered.append(i)
-                continue
+                i = i.replace(".inf", "inf")
             if "null" in i:
-                i.replace("null", "None")
-                filtered.append(i)
-                continue
+                i = i.replace("null", "None")
 
             try:
                 filtered.append(float(i))
